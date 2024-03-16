00c3591fa75ec03bcae2b4f22153057f3d6959a0
joeythornberry <lewis.thornberry@christendom.edu>
2024-03-16 15:42:53 -0400

    [refactor] replace newlines with spaces in preview, to make word counting easier

--- src/assemblePaper.js
+++ src/assemblePaper.js
@@ -1,12 +1,30 @@
 const { splitByEmptyLines } = require('./splitByEmptyLines');
 
+function deNewline(input) {
+	let counter = 0
+	let output = ""
+	while (counter < input.length) {
+		if (input[counter] == "\r") {
+			counter += 1
+		} else if (input[counter] == "\n") {
+			output += " "
+			counter += 1
+		} else {
+			output += input[counter]
+			counter += 1
+		}
+	}
+	return output
+}
+
 function assemblePaper(paragraphs, paragraphDelimiters) { 
 	let paper = ""
 	for(const paragraphFile of paragraphs) {
 		const splitFile = splitByEmptyLines(paragraphFile)
 		for(const paragraph of splitFile) {
 			paper += paragraphDelimiters.start
-			paper += paragraph
+			const deNewlinedParagraph = deNewline(paragraph)
+			paper += deNewlinedParagraph
 			paper += paragraphDelimiters.end
 		}
 	}
