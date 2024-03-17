a58840e279333a0a3e9fa27e8e1579a771519479
joeythornberry <lewis.thornberry@christendom.edu>
2024-03-17 14:32:28 -0400

    [fix] colors actually change for theme

--- src/setTheme.js
+++ src/setTheme.js
@@ -5,11 +5,11 @@ function setTheme(theme) {
 	if (theme === "light") {
 		changes.push({property: "--background-color", value: "#ffffff"})
 		changes.push({property: "--text-color", value: "#000000"})
-		changes.push({property: "--tertiary-color",value: "ff5500"})
+		changes.push({property: "--tertiary-color",value: "#c2c2c2"})
 	} else if (theme === "dark") {
 		changes.push({property: "--background-color", value: "#111111"})
 		changes.push({property: "--text-color", value: "#ffffff"})
-		changes.push({property: "--tertiary-color",value: "0000ff"})
+		changes.push({property: "--tertiary-color",value: "#0000ff"})
 	}
 	for (const change of changes) {
 	document.querySelector(":root").style.setProperty(change.property,change.value)
