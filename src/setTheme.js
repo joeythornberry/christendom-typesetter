af5c54a52876146cf579ac47d8ab8508f51335fe
joeythornberry <lewis.thornberry@christendom.edu>
2024-03-17 21:22:28 -0400

    [fix] make footnote edit color dependent upon theme

--- src/setTheme.js
+++ src/setTheme.js
@@ -6,10 +6,12 @@ function setTheme(theme) {
 		changes.push({property: "--background-color", value: "#ffffff"})
 		changes.push({property: "--text-color", value: "#202020"})
 		changes.push({property: "--tertiary-color",value: "#c2c2c2"})
+		changes.push({property: "--edit-color", value: "#f0f0f0"})
 	} else if (theme === "dark") {
 		changes.push({property: "--background-color", value: "#101010"})
 		changes.push({property: "--text-color", value: "#ffffff"})
 		changes.push({property: "--tertiary-color",value: "#0000c0"})
+		changes.push({property: "--edit-color", value: "#303030"})
 	}
 	for (const change of changes) {
 	document.querySelector(":root").style.setProperty(change.property,change.value)
