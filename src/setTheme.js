e4a924e1b29cfc7d46ef4705d5c1d764a21d5a07
joeythornberry <lewis.thornberry@christendom.edu>
2024-03-17 20:25:48 -0400

    [feature] lower color contrast for dark theme

--- src/setTheme.js
+++ src/setTheme.js
@@ -4,12 +4,12 @@ function setTheme(theme) {
 		console.log("setting theme to", theme)
 	if (theme === "light") {
 		changes.push({property: "--background-color", value: "#ffffff"})
-		changes.push({property: "--text-color", value: "#000000"})
+		changes.push({property: "--text-color", value: "#202020"})
 		changes.push({property: "--tertiary-color",value: "#c2c2c2"})
 	} else if (theme === "dark") {
-		changes.push({property: "--background-color", value: "#111111"})
+		changes.push({property: "--background-color", value: "#101010"})
 		changes.push({property: "--text-color", value: "#ffffff"})
-		changes.push({property: "--tertiary-color",value: "#0000ff"})
+		changes.push({property: "--tertiary-color",value: "#0000c0"})
 	}
 	for (const change of changes) {
 	document.querySelector(":root").style.setProperty(change.property,change.value)
